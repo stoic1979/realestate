06513fd619dcdcd58568d12b2777c7cd691f9685
PR <ranipriyanka713@gmail.com>
2017-10-30 17:56:50 +0530

    code clean

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 
 var express = require("express");
-var app = express();
-var router = express.Router();
-var path = __dirname + '/views/';
+var app     = express();
+var router  = express.Router();
+var path    = __dirname + '/views/';
 
-router.use(function (req,res,next) {
+//----------------------------------------------------------
+//                    Setup app
+//----------------------------------------------------------
+
+router.use(function (req, res, next) {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/",function(req,res){
+router.get("/", function (req,res) {
   res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
+router.get("/about",function (req, res) {
   res.sendFile(path + "about.html");
 });
 
-router.get("/contact",function(req,res){
+router.get("/contact",function (req,res) {
   res.sendFile(path + "contact.html");
 });
 
-app.use("/",router);
+app.use("/", router);
 
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
+app.use("*", function(req,res) {
+  res.sendFile(path  + "404.html");
 });
 
-app.listen(8000,function(){
-  console.log("Live at Port 8000");
+//---------------------------------------------------------------
+//                    START SERVER 
+//---------------------------------------------------------------
+
+app.listen (process.env.PORT || 3000, function() {
+  console.log("Live at Port 3000");
 })
